f15d1bddf29b0502932bd5babf3a4b463da47ec9
Untung <u.untung0029@gmail.com>
2021-12-16 17:51:21 +0700

    Setup react navigation

---
+++ src/pages/employee-add/employee-add.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmployeeAdd = () => {
+  return (
+    <div>
+      <h1>Employee Add!</h1>
+    </div>
+  );
+};
+
+export default EmployeeAdd;
---
+++ src/pages/employee-list/employee-list.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EmployeeList = () => {
+  return (
+    <div>
+      <h1>Employee List!</h1>
+    </div>
+  );
+};
+
+export default EmployeeList;
