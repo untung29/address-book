d692d3232fbcbb01e9540215c6e2cb51bfe2f3f0
Untung <u.untung0029@gmail.com>
2021-12-19 23:08:05 +0700

    Code-splitting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 // Pages
-import EmployeeList from "./pages/employee-list/employee-list";
-import EmployeeAdd from "./pages/employee-add/employee-add";
+// import EmployeeList from "./pages/employee-list/employee-list";
+// import EmployeeAdd from "./pages/employee-add/employee-add";
 
 // Components
 import TopNavigation from "./components/top-navigation/top-navigation.component";
 
+const EmployeeAdd = React.lazy(() => import("./pages/employee-add/employee-add"));
+const EmployeeList = React.lazy(() => import("./pages/employee-list/employee-list"));
 function App() {
   return (
     <div className="container">
       <TopNavigation />
-      <Routes>
-        <Route path="/" element={<EmployeeList />} />
-        <Route path="/add" element={<EmployeeAdd />} />
-      </Routes>
+      <React.Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<EmployeeList />} />
+          <Route path="/add" element={<EmployeeAdd />} />
+        </Routes>
+      </React.Suspense>
     </div>
   );
 }
